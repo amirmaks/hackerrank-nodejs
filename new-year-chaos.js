548e880eb04dcbe06a15f79923ece02ab6a53f3d
Makhsat Amirov <amriovmaks@gmail.com>
2019-01-11 15:36:45 +0600

    new-year-chaos

--- new-year-chaos.js
+++ new-year-chaos.js
@@ -6,7 +6,7 @@ function minimumBribes(q) {
       c = "Too chaotic";
       break;
     }
-    for (let j = Math.max(0, q[p] - 2); j < p; j++) {
+    for (let j = q[p] - 2; j < p; j++) {
       if (q[j] > q[p])
         ++c
     }
@@ -15,10 +15,12 @@ function minimumBribes(q) {
 }
 
 // console.log(minimumBribes([2, 1, 5, 3, 4]));
+
 // console.log(minimumBribes([2, 5, 1, 3, 4]));
 
 // console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])); //7
-minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]); //7
+// minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]); //7
 
 // console.log(minimumBribes([1, 2, 5, 3, 4, 7, 8, 6]));
 // console.log(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]));
+minimumBribes([1, 2, 3, 4, 5, 6, 7, 10, 11, 8, 9,]); //7
