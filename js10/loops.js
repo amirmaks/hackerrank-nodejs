1b9f0689cf2aa1ab1edfc3bfef543ca7ae0abf5f
Makhsat Amirov <amriovmaks@gmail.com>
2019-05-29 09:38:57 +0600

    js 10

---
+++ js10/loops.js
@@ -0,0 +1,25 @@
+function vowelsAndConsonants(s) {
+  const vowels = {
+    'a' : 1,
+    'e' : 1,
+    'i' : 1,
+    'o' : 1,
+    'u' : 1
+  };
+
+  const firstPart = [];
+  const secondPart = [];
+
+  for (let w of s) {
+    if (vowels[w]) {
+      firstPart.push(w);
+    } else {
+      secondPart.push(w);
+    }
+  }
+
+  firstPart.forEach(w => console.log(w));
+  secondPart.forEach(w => console.log(w));
+}
+
+vowelsAndConsonants('javascriptloops')
